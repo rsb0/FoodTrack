e40f7edf9c267a80eb9ed7f53271125a8be912f2
rsb0 <rune.s.brekke@gmail.com>
2020-09-06 17:18:18 +0200

    removed unused Using statements

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers {
+namespace API.Controllers
+{
     [Route ("api/[controller]")]
     [ApiController]
     public class ActivitiesController : ControllerBase {
