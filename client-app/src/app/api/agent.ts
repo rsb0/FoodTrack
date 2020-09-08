061512058ebe98a34171310a1757fff3d2c41209
rsb0 <rune.s.brekke@gmail.com>
2020-09-08 08:01:21 +0200

    react: created new file to hold api calls from client application and defined reusable methods for get, post, put and del

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,29 @@
+import { IActivity } from './../models/activity';
+import axios from 'axios';
+import { request } from 'http';
+
+// File for defining api calls
+// set baseURL
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+// Store request in a constant
+const responseBody = (response: AxiosResponse) => response.data;
+
+// create an object of common reusable requests
+const requests = {
+    get: (url: string) => axios.get(url).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    del: (url: string) => axios.delete(url).then(responseBody)
+};
+
+// Activities object for our activies requests
+const Activities = {
+    list: () => requests.get('/activities'),
+    details: (id: string) => requests.get(`/activities/${id}`),
+    create: (activity: IActivity) => requests.post('/activities', activity),
+    update: (activity: IActivity) => requests.put(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del(`/activities/${id}`)
+};
+
+export default { Activities }
