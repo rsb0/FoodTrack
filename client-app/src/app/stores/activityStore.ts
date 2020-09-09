87df52e4d2deb8fddf6d86a2fc50e1413dba5ab1
rsb0 <rune.s.brekke@gmail.com>
2020-09-09 16:10:03 +0200

    moved create activity-functionality from activityForm and navBar to mobX store

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -8,16 +8,39 @@ class ActivityStore {
     @observable selectedActivity: IActivity | undefined;
     @observable loadingInitial = false;
     @observable editMode = false;
+    @observable submitting = false;
 
-    @action loadActivities = () => {
+    @action loadActivities = async () => {
         this.loadingInitial = true;
-        agent.Activities.list()
-        .then(activities => {
+        try {
+          const activities = await agent.Activities.list();
           activities.forEach((activity) => {
             activity.date = activity.date.split(".")[0];
             this.activities.push(activity);
-          })
-        }).finally(() => this.loadingInitial = false);
+          });
+          this.loadingInitial = false;
+        } catch (error) {
+          console.log(error);
+          this.loadingInitial = false;
+        }
+    };
+
+    @action createActivity = async (activity: IActivity) => {
+      this.submitting = true;
+      try {
+        await agent.Activities.create(activity);
+        this.activities.push(activity);
+        this.editMode = false;
+        this.submitting = false;
+      } catch (error) {
+        this.submitting = false;
+        console.log(error);
+      };
+    };
+
+    @action openCreateForm = () => {
+      this.editMode = true;
+      this.selectedActivity = undefined;
     }
 
     @action selectActivity = (id: string) => {
